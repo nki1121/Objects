ef6ecbbca69402b3b69555cd97d7a0040aa824c4
nki1121 <nkiuniv@gmail.com>
2024-03-26 21:47:31 +0900

    ボタン

--- Part4/ContentView.swift
+++ Part4/ContentView.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Button(action: {
+            
+        }, label: {
+            Text("ボタン")
+        })
         .padding()
     }
 }
