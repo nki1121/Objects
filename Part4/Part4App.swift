a1d3dc17f78213539ba24ec35947339b211e151b
nki1121 <nkiuniv@gmail.com>
2024-03-26 21:40:52 +0900

    Initial Commit

---
+++ Part4/Part4App.swift
@@ -0,0 +1,17 @@
+//
+//  Part4App.swift
+//  Part4
+//
+//  Created by 中井晋平 on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Part4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
